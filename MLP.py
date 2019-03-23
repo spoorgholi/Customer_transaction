1e85eb489627ae00a805c132cc0f63911123531a
Soroosh <soroosh_99@yahoo.com>
2019-03-23 08:23:49 +0100

    4000 epochs with 64 percent acc

--- MLP.py
+++ MLP.py
@@ -4,6 +4,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from itertools import chain
 import seaborn as sns
 
 #importing the dataset
@@ -36,11 +37,12 @@ from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 x_train = sc.fit_transform(x_train)
 x_test = sc.transform(x_test)
-
+kaggle = sc.transform(kaggle)
 
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
+from keras.constraints import UnitNorm
 '''
 #initilizing the ANN
 classifier = Sequential()
@@ -78,40 +80,60 @@ print("Accuracy on a validation set is : \n", acc_percent, "%" )
 #evaluating the ann with k-fold cross validation using the keras wrapper
 from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import cross_val_score
+from keras.utils import plot_model
 
 def build_classifier():
     
     classifier = Sequential()
-    classifier.add(Dense(units = 100, activation = 'relu', kernel_initializer = 'uniform', input_dim = 200))
+    classifier.add(Dense(units = 200, activation = 'relu', kernel_initializer = 'normal', input_dim = 200, 
+                         kernel_constraint = UnitNorm()))
     classifier.add(Dropout(p = 0.2))
-    classifier.add(Dense(units = 100, activation = 'relu', kernel_initializer = 'uniform'))
+    classifier.add(Dense(units = 100, activation = 'relu', kernel_initializer = 'normal', 
+                         kernel_constraint = UnitNorm()))
     classifier.add(Dropout(p = 0.2))
-    classifier.add(Dense(units = 100, activation = 'relu', kernel_initializer = 'uniform'))
+    classifier.add(Dense(units = 50, activation = 'relu', kernel_initializer = 'normal', 
+                         kernel_constraint = UnitNorm()))
     classifier.add(Dropout(p = 0.2))
-    classifier.add(Dense(units = 1, activation = 'sigmoid', kernel_initializer = 'uniform'))
+    classifier.add(Dense(units = 1, activation = 'sigmoid', kernel_initializer = 'normal', 
+                         kernel_constraint = UnitNorm()))
     classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+    plot_model(classifier, to_file='model.png')
     
     return classifier
 
-classifier = KerasClassifier(build_fn = build_classifier, batch_size = 250, epochs = 100)
-accuricies = cross_val_score(estimator = classifier, X = x_train, y = y_train, cv = 10, n_jobs = -1)
-mean = accuricies.mean()
-variance = accuricies.std()
-print(mean, "\n", variance)
+classifier = KerasClassifier(build_fn = build_classifier, batch_size = 250, epochs = 1000)
+################### K-fold cross calidation ########################
+#accuricies = cross_val_score(estimator = classifier, X = x_train, y = y_train, cv = 2, n_jobs = 1)
+#mean = accuricies.mean() 
+#variance = accuricies.std() 
+#print('Base Model:\n', 'mean is %2f' %(mean), "\n", "variance is %2f" %(variance))
 
-#predicting the first results for the competiotion 
-kaggle = sc.fit_transform(kaggle)
 # Fitting the ANN to the Training set
-history_callback = classifier.fit(x_train, y_train, batch_size = 250, epochs = 2)
+history_callback = classifier.fit(x_train, y_train, batch_size = 250, epochs = 4000)
 loss_history = history_callback.history['loss']
-plt.plot(np.arange(0, len(loss_history)), loss_history)
+########################### Live Plot ########################
+
+######################### Output ###############################
+plt.figure()
+plt.plot(np.arange(1, len(loss_history) + 1), loss_history)
+plt.xlabel("epochs")
+plt.ylabel("loss")
+plt.savefig('Loss_epoch.png')
 plt.show()
+
 y_final = classifier.predict(kaggle)
-#y_final = (y_final > 0.5)
+unique, counts = np.unique(y_final, return_counts=True)
+y_dict = dict(zip(unique, counts))
+'''
 for i in range(0, len(y_final)):
     if y_final[i] <= 0.5:
         y_final[i] = 0
     else:
         y_final[i] = 1
-y_final = y_final.astype(int)
-np.savetxt("output.csv", y_final, delimiter=",")
+        
+'''
+y_final = list(chain(*y_final))
+#write the outpu
+sample = pd.read_csv("sample_submission.csv")
+y_final = pd.DataFrame({'ID_code': sample['ID_code'] , 'target': y_final})
+y_final.to_csv('output.csv', sep = ',', index = False)
